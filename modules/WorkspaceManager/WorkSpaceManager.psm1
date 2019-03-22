812add4dd2ef90e21bafb091846f0ffe61ea5aa1
Jake Berg <jake.berg@method4.co.uk>
2019-03-22 14:39:38 +0000

    Add command to close workspaces (up to 10)

--- modules/WorkspaceManager/WorkSpaceManager.psm1
+++ modules/WorkspaceManager/WorkSpaceManager.psm1
@@ -18,6 +18,26 @@
     $KeyShortcut::CreateVirtualDesktopInWin10()
 }
 
+function CloseVirtualDeskTop {
+    $KeyShortcut = Add-Type -MemberDefinition @" 
+    [DllImport("user32.dll")] 
+    static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo); 
+    //WIN + CTRL + D: Create a new desktop 
+    public static void CreateVirtualDesktopInWin10() 
+    { 
+        //Key down 
+        keybd_event((byte)0x5B, 0, 0, UIntPtr.Zero); //Left Windows key  
+        keybd_event((byte)0x11, 0, 0, UIntPtr.Zero); //CTRL 
+        keybd_event((byte)0x73, 0, 0, UIntPtr.Zero); //W
+        //Key up 
+        keybd_event((byte)0x5B, 0, (uint)0x2, UIntPtr.Zero); 
+        keybd_event((byte)0x11, 0, (uint)0x2, UIntPtr.Zero); 
+        keybd_event((byte)0x44, 0, (uint)0x2, UIntPtr.Zero); 
+    } 
+"@ -Name CreateVirtualDesktop -UsingNamespace System.Threading -PassThru 
+    $KeyShortcut::CreateVirtualDesktopInWin10()
+}
+
 function ShowMenu {
     Param(
         [Parameter(Mandatory = $true)] $OptionsList,
@@ -148,5 +168,13 @@ function Open-WorkSpace {
     }
 }
 
+function Close-Workspaces {
+    for ($i = 0; $i -lt 10; $i++) {
+        CloseVirtualDeskTop
+        Start-Sleep -Milliseconds 100
+    }
+}
+
 Export-ModuleMember Edit-WorkSpaces
 Export-ModuleMember Open-WorkSpace
+Export-ModuleMember Close-Workspaces
