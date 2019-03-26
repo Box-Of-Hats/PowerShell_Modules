7e383b0aa48fe9df8d173c4fc60bb74d94bf96bf
Jake Berg <jake.berg@method4.co.uk>
2019-03-26 16:31:19 +0000

    Add max length check to lorum text

--- modules/LorumIpsum/LorumIpsum.psm1
+++ modules/LorumIpsum/LorumIpsum.psm1
@@ -8,7 +8,7 @@ function New-LorumIpsum {
     Etiam id dictum neque, et posuere metus. Etiam facilisis eget diam in finibus. Nullam eget sem lacinia, blandit elit quis, dapibus est. Sed commodo consectetur leo, eu ultricies velit imperdiet eu. Sed pharetra nisl id ante sagittis pellentesque. Cras luctus varius dolor eget ullamcorper. Nullam nec est congue, posuere ligula nec, lobortis felis. Aenean at lobortis mi.
     Pellentesque facilisis ultrices enim a ultricies. Duis quis risus vel tortor consectetur condimentum. Nulla nec magna at magna pharetra bibendum. Curabitur suscipit convallis orci ut pretium. Nunc semper purus bibendum eros laoreet efficitur. In venenatis turpis lorem, vitae elementum mauris egestas egestas. Ut efficitur lobortis massa eget pellentesque. Curabitur et fermentum turpis, id consectetur dolor. Duis ut ultrices purus. Curabitur id scelerisque orci. Sed lectus leo, consectetur eget molestie bibendum, sollicitudin a felis. Sed magna est, condimentum ac nisl sit amet, gravida tincidunt lectus. Pellentesque id enim varius, consequat ipsum a, accumsan mi."
 
-    if ($CharLength -gt 0){
+    if ($CharLength -gt 0 -and $CharLength -lt $text.Length){
         $out = $text.Substring(0, $CharLength) 
     } else {
         $out = $text
