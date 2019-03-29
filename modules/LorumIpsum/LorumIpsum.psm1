8926a1ceceba2c92ac27dd8c113a0c55272691ce
Jake Berg <jake.berg@method4.co.uk>
2019-03-29 14:29:11 +0000

    Add custom wordlist support to lorum ipsum

--- modules/LorumIpsum/LorumIpsum.psm1
+++ modules/LorumIpsum/LorumIpsum.psm1
@@ -2,23 +2,27 @@ function New-LorumIpsum {
     Param(
         [int] $CharLength = 150,
         [int] $SentenceLengthWords = 10,
-        [switch] $ExactLength
+        [switch] $ExactLength,
+        $WordList
     )
-    $words = @(
-        "lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "Curabitur", "dignissim", "tortor",
-        "non", "sollicitudin", "fermentum", "Aliquam", "est", "mi", "consectetur", "cursus", "Ut", "pellentesque", "varius",
-        "orci", "quis", "lacinia", "eros", "dictum", "nec", "Donec", "blandit", "rutrum", "leo", "Vestibulum", "cursus",
-        "vulputate", "velit", "a", "tincidunt", "Nam", "mattis", "iaculis", "faucibus", "Nam", "orci", "enim", "feugiat",
-        "at", "molestie", "sed", "tincidunt", "sapien", "Etiam", "hendrerit", "risus", "ullamcorper", "sapien", "convallis",
-        "sit", "amet", "sodales", "nulla", "elementum", "Interdum", "malesuada", "fames", "et", "ac", "ante", "ipsum", "primis",
-        "in", "faucibus", "Phasellus", "gravida", "justo", "non", "pretium", "fermentum", "tellus", "felis", "vehicula", "felis",
-        "iaculis", "nisl", "sem", "feugiat"
-    )
+    
+    if ($null -eq $WordList ) {
+        $WordList = @(
+            "lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "Curabitur", "dignissim", "tortor",
+            "non", "sollicitudin", "fermentum", "Aliquam", "est", "mi", "consectetur", "cursus", "Ut", "pellentesque", "varius",
+            "orci", "quis", "lacinia", "eros", "dictum", "nec", "Donec", "blandit", "rutrum", "leo", "Vestibulum", "cursus",
+            "vulputate", "velit", "a", "tincidunt", "Nam", "mattis", "iaculis", "faucibus", "Nam", "orci", "enim", "feugiat",
+            "at", "molestie", "sed", "tincidunt", "sapien", "Etiam", "hendrerit", "risus", "ullamcorper", "sapien", "convallis",
+            "sit", "amet", "sodales", "nulla", "elementum", "Interdum", "malesuada", "fames", "et", "ac", "ante", "ipsum", "primis",
+            "in", "faucibus", "Phasellus", "gravida", "justo", "non", "pretium", "fermentum", "tellus", "felis", "vehicula", "felis",
+            "iaculis", "nisl", "sem", "feugiat"
+        )
+    }
 
     $output = ""
     $currentSentenceLength = 0
     while ($output.Length -le $CharLength) {
-        $randomWord = Get-Random -InputObject $words
+        $randomWord = Get-Random -InputObject $WordList
 
         # Capitalise first word of each sentence
         if ($currentSentenceLength -eq 0) {
@@ -28,10 +32,10 @@ function New-LorumIpsum {
             $randomWord = $randomWord.ToLower()
         }
 
-        $output = "$output $randomWord"
+        $output = "$output $randomWord".TrimStart(' ')
 
         # End each setence with a period
-        if ($currentSentenceLength -ge $SentenceLengthWords-1) {
+        if ($currentSentenceLength -ge $SentenceLengthWords - 1) {
             $output = "$output."
             $currentSentenceLength = 0
 
@@ -44,7 +48,7 @@ function New-LorumIpsum {
     # Truncate string if required
     if ($ExactLength) {
         if ($output.Length -ge $CharLength) {
-            $output = $output.Substring(0, $CharLength-1)
+            $output = $output.Substring(0, $CharLength - 1)
         }
     }
 
@@ -52,7 +56,7 @@ function New-LorumIpsum {
     if ($output.Substring($output.Length - 1) -ne ".") {
         $output = "$output."
     }
-
+    Write-Host "Copied to clipboard." -ForegroundColor Green
     $output | clip
     return $output
 }
