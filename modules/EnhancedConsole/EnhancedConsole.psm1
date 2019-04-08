c59792e8684514a5ef1d0926ff74926ab97975c1
Jake Berg <jake.berg@method4.co.uk>
2019-04-08 14:46:54 +0100

    Add "ls -l" style command

--- modules/EnhancedConsole/EnhancedConsole.psm1
+++ modules/EnhancedConsole/EnhancedConsole.psm1
@@ -29,12 +29,43 @@ function Set-LocationAndGetChildItem {
     Get-ChildItem
 }
 
+function Get-ChildItemGridView {
+    $children = Get-ChildItem | Select-Object -ExpandProperty Name
+
+    $maxColCount = 3
+    $colCount = 0
+    foreach ($item in $children) {
+        $colCount++
+
+        
+        if ($item.Length -ge 36) {
+            $item = $item.SubString(0, 35) + "…"
+        }
+
+        Write-Host $item -NoNewline
+        
+        $spacesRequired = 40 - $item.Length
+        
+        if ($colCount -ge $maxColCount) {
+            Write-Host ""
+            $colCount = 0
+        }
+        else {
+            for ($i = 0; $i -lt $spacesRequired; $i++) {
+                Write-Host " " -NoNewline
+            }
+        }
+    }
+    Write-Host ""
+}
+
 #Alias
 New-Alias -Name cdl -Value Set-LocationAndGetChildItem
 New-Alias -Name cl -Value Copy-Location
-
+New-Alias -Name lsl -Value Get-ChildItemGridView
 #Exports
 Export-ModuleMember -Alias *
 
 Export-ModuleMember Copy-Location
 Export-ModuleMember Set-LocationAndGetChildItem
+Export-ModuleMember Get-ChildItemGridView
