d9fa9dc5b87a0062aea5a8019d2d55fba1850a84
Box-Of-Hats <ja1ke@hotmail.co.uk>
2019-04-09 22:48:48 +0100

    Add arrow selection menu

--- modules/EasyUI/EasyUI.psm1
+++ modules/EasyUI/EasyUI.psm1
@@ -58,5 +58,68 @@ function Get-Choice {
     return $KeyList.IndexOf($userIn);
 }
 
+<#
+.DESCRIPTION
+Get a choice from a selection, via an arrow-navigated menu.
+
+.PARAMETER OptionsList
+The list of options to choose from
+
+.OUTPUTS
+The index of the selected option
+#>
+
+function Get-ChoiceWithArrowMenu {
+    Param(
+        [Parameter(Mandatory = $true)] $OptionsList,
+        [Parameter(Mandatory = $false)] $Message = "Make a choice:"
+    )
+
+    $cursorLocation = 0;
+    while ($true) {
+        Clear-Host
+        Write-Host $Message
+        for ($i = 0; $i -lt $OptionsList.Count; $i++) {
+            if ($i -eq $cursorLocation) {
+                Write-Host "$($OptionsList[$i]) <" -ForegroundColor Green
+            }
+            else {
+                Write-Host $OptionsList[$i]
+            }
+        }
+        $pressedKeyCode = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Select-Object -ExpandProperty VirtualKeyCode
+        if ($pressedKeyCode -eq 38) {
+            # Up
+            $cursorLocation--
+        }
+        elseif ($pressedKeyCode -eq 40) {
+            # Down
+            $cursorLocation++
+        }
+        elseif ($pressedKeyCode -eq 37) {
+            # Left
+            $cursorLocation = 0
+        }
+        elseif ($pressedKeyCode -eq 39) {
+            # Right
+            $cursorLocation = $OptionsList.Count - 1
+        }
+        elseif ($pressedKeyCode -eq 32) {
+            # Space
+            return $cursorLocation
+        }
+        elseif ($pressedKeyCode -eq 13) {
+            # Return
+            return $cursorLocation
+        }
+        elseif ($pressedKeyCode -eq 27) {
+            # Escape
+            return -1
+        }
+        $cursorLocation = [math]::Min([math]::Max($cursorLocation, 0), $OptionsList.Count - 1)
+    }
+}
+
+Export-ModuleMember Get-ChoiceWithArrowMenu
 Export-ModuleMember Get-Choice
 Export-ModuleMember Get-PinConfirmation
