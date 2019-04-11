7e731aab011af3f5e72175a205725150eae2e111
Box-Of-Hats <ja1ke@hotmail.co.uk>
2019-04-11 23:05:29 +0100

    Add Resize-Image command

--- modules/ImageManipulation/ImageManipulation.psm1
+++ modules/ImageManipulation/ImageManipulation.psm1
@@ -97,4 +97,65 @@ function Convert-SvgToPng {
     
 }
 
+<#
+.SYNOPSIS
+Scale an image.
+
+.DESCRIPTION
+Take an image and scale it up or down by a given multiplier. Then save it to a new file.
+
+.PARAMETER ImagePath
+The image that should be scaled.
+
+.PARAMETER ScaleFactor
+The multiplier to use for the new image size.
+
+.PARAMETER OutImagePath
+The path of the image file to be created.
+
+.EXAMPLE
+Scale-Image -ImagePath .\4x4.png -ScaleFactor 2 -OutImagePath .\8x8.png
+#>
+function Resize-Image {
+    param (
+        [Parameter(Mandatory=$true)]$ImagePath,
+        [Parameter(Mandatory=$true)]$ScaleFactor,
+        [Parameter(Mandatory=$false)]$OutImagePath = $null
+    )
+    
+    if (-not (Test-Path $ImagePath)){
+        Write-Host "Could not find file: $ImagePath" -ForegroundColor Red
+        return $null
+    }
+
+    $ImagePath = Resolve-Path $ImagePath
+
+    if ($null -eq $OutImagePath){
+        $item = Get-ChildItem $ImagePath
+        $OutImagePath = $item.BaseName + "_scaled$ScaleFactor" + $item.Extension
+    }
+
+    $OutImagePath = Join-Path (Get-Location) $OutImagePath
+
+    if (Test-Path $OutImagePath){
+        Write-Host "File already exists: $OutImagePath" -ForegroundColor Red
+        return $null
+    }
+
+    $image = New-Object -ComObject Wia.ImageFile
+    $image.LoadFile($ImagePath)
+
+    $imageProcess = New-Object -ComObject Wia.ImageProcess
+    $imageProcess.Filters.Add($imageProcess.FilterInfos("Scale").FilterId)
+
+    $imageProcess.Filters[1].Properties("MaximumWidth").Value = "$([int]::parse($image.Width * $ScaleFactor))"
+    $imageProcess.Filters[1].Properties("MaximumHeight").Value = "$([int]::parse($image.Height * $ScaleFactor))"
+    $imageProcess.Filters[1].Properties("PreserveAspectRatio").Value = 1
+
+    $scaledImage = $imageProcess.Apply($image)
+    $scaledImage.SaveFile($OutImagePath)
+
+}
+
+Export-ModuleMember Resize-Image
 Export-ModuleMember Convert-SvgToPng
