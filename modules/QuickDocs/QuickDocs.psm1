84bc420f3330bec9ed96e5e1af1847dab3907f56
Jake Berg <jake.berg@method4.co.uk>
2019-11-29 14:17:51 +0000

    Add OpenFile flag to New-Standup

--- modules/QuickDocs/QuickDocs.psm1
+++ modules/QuickDocs/QuickDocs.psm1
@@ -23,10 +23,14 @@ $standupTemplate = '# {0} - {1}
 
 function New-Standup() {
     param(
-        [Parameter(Mandatory = $true)][string]$ProjectName
+        [Parameter(Mandatory = $true)][string]$ProjectName,
+        [Parameter()][switch]$OpenFile
     )
     $today = ([datetime]::Now).ToString("yyyy-MM-dd")
     $path = New-Item -Type File -Path ("{0}-{1}.md" -F ($today, $ProjectName)) -Value ($standupTemplate -F ($ProjectName, $today))
+    if ($OpenFile){
+        code $path
+    }
     return $path.FullName
 }
 
